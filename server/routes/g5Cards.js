332daabfa548eed1e240ff0b3b7eeac990fa989a
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-08-23 13:16:33 -0400

    Creating the routers of "g5Boxes" and "g5Cards".

---
+++ server/routes/g5Cards.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const g5CardsController = require("../controllers/g5Cards");
+const catchAsync = require("../middleware/catchAsync");
+const { isLoggedIn } = require("../middleware/isLoggedIn");
+// GET all cards belonging to a specific box
+router.get("/:boxId/cards", isLoggedIn, catchAsync(g5CardsController.index));
+// CREATE a new card inside a specific box
+router.post("/:boxId/cards", isLoggedIn, catchAsync(g5CardsController.create));
+// UPDATE a card
+router.put("/:id", isLoggedIn, catchAsync(g5CardsController.update));
+// DELETE a card
+router.delete("/:id", isLoggedIn, catchAsync(g5CardsController.remove));
+
+module.exports = router;
