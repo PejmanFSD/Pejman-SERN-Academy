2edd85e48ceff2bcc425e256b17b3f4e3a90fcf8
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-09-04 17:26:53 -0400

    Hiding/Showing the answer of each card.
    For each card, a button is created that shows the answer.

--- client/src/Components/g5/Card.jsx
+++ client/src/Components/g5/Card.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export default function Card({question, answer, boxNumber}) {
+    const [isAnswerRevealed, setIsAnswerRevealed] = useState(false);
+
+    const RevealTheAnswer = () => {
+        setIsAnswerRevealed(currIsAnswerRevealed => !currIsAnswerRevealed);
+    }
     return (
         <div>
             <div>Question: {question}</div>
-            <div>Answer: {answer}</div>
+            {!isAnswerRevealed ?
+                <button onClick={RevealTheAnswer}>Reveal the answer</button> :
+                <div>Answer: {answer}</div>
+            }
             <div>Box: {boxNumber}</div>
+            -----------------------
         </div>
     )
 }
